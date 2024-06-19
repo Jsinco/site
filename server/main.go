e207f736e1da0f31e8ba146c5f907668594b20bc
Jsinco <jonahmarksinclair@gmail.com>
2024-06-18 20:24:32 -0400

    init

--- server/main.go
+++ server/main.go
@@ -5,17 +5,30 @@ import (
 	"net/http"
 )
 
+func errorHandler(status int) bool {
+	if status == http.StatusNotFound {
+		return true
+	}
+	return false
+}
+
 func main() {
 	// Define the directory containing static files
 	staticDir := http.Dir("./")
 
 	// Create a file server handler for the static directory
 	fileServer := http.FileServer(staticDir)
+	fileServerr := http.FileServer(http.Dir("./images"))
 
 	// Handle all requests using the file server
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// This handler serves all requests from the static directory
 		fileServer.ServeHTTP(w, r)
+		fileServerr.ServeHTTP(w, r)
+		if errorHandler(http.StatusNotFound) {
+			http.ServeFile(w, r, "404.html")
+			return
+		}
 	}))
 
 	// Start the server
