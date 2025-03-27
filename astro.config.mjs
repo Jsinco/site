1cb1eb8c1f9ae16805a045411e85f4cd13e79402
Jsinco <jonah@jsinco.dev>
2025-03-27 01:23:26 -0400

    Add 404 page, cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jsinco.dev',
-    //base: 'site',
     vite: {
         plugins: [tailwindcss()],
     },
--- prettier.config.js
+++ prettier.config.js
@@ -3,10 +3,13 @@
  * @type {import("prettier").Config}
  */
 const config = {
+  plugins: ["prettier-plugin-astro"],
   trailingComma: "es5",
   tabWidth: 4,
   semi: false,
   singleQuote: true,
+  printWidth: 999,
+  htmlWhitespaceSensitivity: "ignore",
 };
 
 export default config;
